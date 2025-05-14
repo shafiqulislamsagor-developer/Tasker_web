ba89ca03bc338c1e522b6adab417d192fb676a19
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-14 15:11:46 +0600

    component create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import BeginHero from "./components/BeginHero";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import TaskBoard from "./components/task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
-      <BeginHero />
+      <div className="flex items-center flex-col justify-center">
+        <BeginHero />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
